3cfbc97b044ee79330e9bcbfc37a0c370ac4c8b2
jsonwamuyu <jysonmuchiri@gmail.com>
2023-01-20 05:50:11 -0800

    initial comment

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import {logo} from './assets'
+
+const App = () => {
+  return (
+    <h4 className="text-blue-900 font-semibold text-3xl">Hello world</h4>
+  )
+}
+
+export default App
